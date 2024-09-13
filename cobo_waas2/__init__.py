0a6d7605001b45eddcf0f494c60323c1cb814e27
ZhangJun <5784767+zhangjun725@users.noreply.github.com>
2024-09-13 15:37:01 +0800

    release v1.3.1

--- cobo_waas2/__init__.py
+++ cobo_waas2/__init__.py
@@ -12,7 +12,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 # import apis into sdk package
 from cobo_waas2.api.developers_webhooks_api import DevelopersWebhooksApi
--- setup.py
+++ setup.py
@@ -19,14 +19,14 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "cobo-waas2"
-VERSION = "1.3.0"
+VERSION = "1.3.1"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",
     "python-dateutil",
     "pydantic >= 2",
     "typing-extensions >= 4.7.1",
-    "ed25519==1.5"
+    "PyNaCl==1.5.0"
 ]
 
 with open("README.md", "r", encoding="utf-8") as fh:
